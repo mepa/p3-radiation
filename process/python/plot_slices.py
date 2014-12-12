e02d1e0c56a61b67588c1feba555c99c8d933762
Meghann Agarwal <mepa@physics.utexas.edu>
2014-12-12 12:12:00 -0600

    Updating plot_slices.py.

--- process/python/plot_slices.py
+++ process/python/plot_slices.py
@@ -1,14 +1,21 @@
-from yt.mods import * # set up our namespace
+import yt
+from yt.units import kpc
 
+dirprefix = "/scratch/cerberus/d4/mepa/"
 #dirname = "/scratch/01707/mepa/Rad_1Mpc/RadCosmoLW_res128/"
-dirname = "/scratch/cerberus/d4/mepa/data/RadCosmo_res128/stampede/"
-filename = "radCosmoLW_hdf5_chk_0377"
+dirname = dirprefix + "data/RadCosmo_res128/stampede/"
+filename = "radCosmoLW_hdf5_chk_0342"
 fn = dirname + filename # parameter file to load
 
-pf = load(fn) # load data
-pc = PlotCollection(pf) # defaults to center at most dense point
-pc.add_slice("Density", 0) # 0 = x-axis
-pc.add_slice("Density", 1) # 1 = y-axis
-pc.add_slice("Density", 2) # 2 = z-axis
-pc.set_width(1.5, 'mpc') # change width of all plots in pc
-pc.save(fn) # save all plots
+ds = yt.load(fn) # load data
+#slc = yt.SlicePlot(ds, "x", "density")
+#slc.zoom(10)
+#slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
+slc = yt.SlicePlot(ds, "y", "density", center='m')
+#slc.zoom(10)
+slc.set_width(6*kpc)
+slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
+#slc = yt.SlicePlot(ds, "z", "density")
+#slc.zoom(10)
+#slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
+
