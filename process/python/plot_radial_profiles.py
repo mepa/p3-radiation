dab7cf66579ff07943b55742dac695d6b4a14353
Meghann Agarwal <mepa@physics.utexas.edu>
2014-12-12 12:49:45 -0600

    Working on chk 381. TODO: pass chk file as input param.

--- process/python/plot_radial_profiles.py
+++ process/python/plot_radial_profiles.py
@@ -1,14 +1,17 @@
 # TODO: implement command-line chk file numbers
-dirname = "/scratch/01707/mepa/Rad_1Mpc/RadCosmoLW_res128/"
-filename = "radCosmoLW_hdf5_chk_0377"
+dirprefix = "/scratch/cerberus/d4/mepa/"
+#dirname = "/scratch/01707/mepa/Rad_1Mpc/RadCosmoLW_res128/"
+dirname = dirprefix + "data/RadCosmo_res128/stampede/"
+filename = "radCosmoLW_hdf5_chk_0381"
+fn = dirname + filename # parameter file to load
 
 # Load the dataset.
-ds = yt.load(dirname + filename)
+ds = yt.load(fn)
 
 v, c = pf.h.find_max("Density")
 print "Found highest density of %0.3e at %s" % (v, c)
 
-# Create a sphere of radius 100 kpc in the center of the box.
+# Create a sphere of radius 100 kpc around the cell of max density.
 my_sphere = ds.sphere(c, (100.0, "kpc"))
 
 # Create a profile of the average density vs. radius.
@@ -21,4 +24,4 @@ plot.set_unit('radius', 'kpc')
 # Save the image.
 # Optionally, give a string as an argument
 # to name files with a keyword.
-plot.save()
+plot.save(dirprefix + 'process/python/plots/' + filename)
--- process/python/plot_slices.py
+++ process/python/plot_slices.py
@@ -1,21 +1,22 @@
 import yt
 from yt.units import kpc
 
+
 dirprefix = "/scratch/cerberus/d4/mepa/"
 #dirname = "/scratch/01707/mepa/Rad_1Mpc/RadCosmoLW_res128/"
 dirname = dirprefix + "data/RadCosmo_res128/stampede/"
-filename = "radCosmoLW_hdf5_chk_0342"
+filename = "radCosmoLW_hdf5_chk_0381"
 fn = dirname + filename # parameter file to load
 
 ds = yt.load(fn) # load data
-#slc = yt.SlicePlot(ds, "x", "density")
-#slc.zoom(10)
-#slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
-slc = yt.SlicePlot(ds, "y", "density", center='m')
-#slc.zoom(10)
-slc.set_width(6*kpc)
+slc = yt.SlicePlot(ds, "x", "density")
+slc.zoom(10)
 slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
-#slc = yt.SlicePlot(ds, "z", "density")
-#slc.zoom(10)
-#slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
+slc = yt.SlicePlot(ds, "y", "density", center='m')
+slc.zoom(10)
+#slc.set_width(6*kpc)
+slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
+slc = yt.SlicePlot(ds, "z", "density")
+slc.zoom(10)
+slc.save(dirprefix + 'process/python/plots/zoom_' + filename)
 
